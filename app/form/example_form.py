4a425ab6f8e73ec8127cf1986f2a6597cf039213
Andre Chris Abad <andreabad17@gmail.com>
2024-05-17 11:47:08 -0700

    Added post / fomr data validation

---
+++ app/form/example_form.py
@@ -0,0 +1,11 @@
+# from flask_wtf import FlaskForm
+# from wtforms import IntegerField,DecimalField,TextAreaField,BooleanField
+# from wtforms.validators import DataRequired,ValidationError
+# from app.models import < Model Name > 
+# 
+# class < Model Name > (FlaskForm):
+#     string= TextAreaField("name",validators=[DataRequired(),cereal_exists])
+#     isBool= BooleanField("type",validators=[DataRequired()])
+#     integer= IntegerField("calories",validators=[DataRequired()])
+#     decimal= DecimalField("protein",validators=[DataRequired()])
+    
